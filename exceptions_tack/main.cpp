5f11779feba2b85653c3272d9b0e2d4df9229127
mira318 <chuvilyaeva999@yandex.ru>
2022-04-07 07:07:22 +0300

    END_CATCH as solution?

--- exceptions_tack/main.cpp
+++ exceptions_tack/main.cpp
@@ -2,8 +2,11 @@
 #include <csetjmp>
 
 const int MAX_BUFFERS_NUM = 1e5;
+const int MAX_EXCEPTIONS_STACKED = 1e5;
 int last_env = -1;
+int last_exception = -1;
 std::jmp_buf env_buffers[MAX_BUFFERS_NUM];
+int exception_stack[MAX_EXCEPTIONS_STACKED];
 int exception = -1, checking_exception = -1;
 
 #define TRY last_env++;                                                                          \
@@ -11,9 +14,14 @@ int exception = -1, checking_exception = -1;
             if(exception == 0)
 
 #define THROW(a) exception = a;                                                                  \
+                 last_exception++;                                                                \
+                 exception_stack[last_exception] = a;                                            \
                  std::longjmp(env_buffers[last_env], exception);
 
-#define RETHROW std::longjmp(env_buffers[last_env], exception);
+#define RETHROW if(last_exception < 0){                                                          \
+                    std::cout << "Terminated: rethrowning was impossible" << std::endl;          \
+                }                                                                                \
+                std::longjmp(env_buffers[last_env], exception_stack[last_exception]);
 
 #define CATCH(b) else {                                                                          \
                      checking_exception = b;                                                     \
@@ -25,6 +33,9 @@ int exception = -1, checking_exception = -1;
                          }                                                                       \
                          std::longjmp(env_buffers[last_env], exception);                         \
                      }
+
+#define END_CATCH last_exception--;                                                              \
+                 }
 int main(){
     /* How it should be ... */
     last_env++;
@@ -93,37 +104,58 @@ int main(){
     }
     std::cout << "Gone through experimental try-catch" << std::endl;
 
+    /* Harder example without END_CATCH */
     TRY {
         TRY {
             TRY {
                 TRY {
                     THROW(11)
-
                 } CATCH(7)
                     std::cout << "Catching 7" << std::endl;
                 }
-
             } CATCH(11)
                 std::cout << "Catching 11" << std::endl;
                 TRY {
                     THROW(13)
-
                 } CATCH(13)
                     std::cout << "Catching inner 13" << std::endl;
                 }
-
                 RETHROW
             }
-
         } CATCH(11)
             std::cout << "Catching rethrown 11" << std::endl;
             THROW(7)
-
         }
-
     } CATCH(7)
         std::cout << "Caught 7 as final" << std::endl;
     }
     std::cout << "Gone through harder example" << std::endl;
+
+    /* Harder example with END_CATCH */
+    TRY {
+        TRY {
+            TRY {
+                TRY {
+                    THROW(11)
+                } CATCH(7)
+                    std::cout << "Catching 7" << std::endl;
+                END_CATCH
+            } CATCH(11)
+                std::cout << "Catching 11" << std::endl;
+                TRY {
+                    THROW(13)
+                } CATCH(13)
+                    std::cout << "Catching inner 13" << std::endl;
+                END_CATCH
+                RETHROW
+            END_CATCH
+        } CATCH(11)
+            std::cout << "Catching rethrown 11" << std::endl;
+            THROW(7)
+        END_CATCH
+    } CATCH(7)
+        std::cout << "Caught 7 as final" << std::endl;
+    END_CATCH
+    std::cout << "Gone through harder example, using END_CATCH" << std::endl;
     return 0;
 }
